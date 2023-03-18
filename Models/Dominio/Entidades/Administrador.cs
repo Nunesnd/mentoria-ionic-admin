b0fbc4dbe4e6fd7121d6e2eb51f75dfa17c46f62
Diego Nunes <nunesdiegond@gmail.com>
2023-03-18 01:07:25 -0300

    erros de digitaçao corrigidos, páginas ciradas, funcionando cdastramento de administradores

--- Models/Dominio/Entidades/Administrador.cs
+++ Models/Dominio/Entidades/Administrador.cs
@@ -1,10 +1,8 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Linq;
 
 namespace adminMeuApp.Models.Dominio.Entidades
 {
-    public class Adminstrador
+    public class Administrador
     {
         [Required]
         [Key]
--- Models/Infraestrutura/Database/BancoContexto.cs
+++ Models/Infraestrutura/Database/BancoContexto.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Linq;
 using adminMeuApp.Models.Dominio.Entidades;
 
 namespace adminMeuApp.Models.Infraestrutura.Database
@@ -7,7 +6,7 @@ namespace adminMeuApp.Models.Infraestrutura.Database
     public class BancoContexto : DbContext
     {
         public BancoContexto(DbContextOptions<BancoContexto> options) : base(options) { }
-        public DbSet<Adminstrador> Administradores { get; set; }
+        public DbSet<Administrador> Administradores { get; set; }
 
         /*
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
